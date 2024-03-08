48313df45172c83925a480f4180d642440e75990
mengkim168 <mengkimtang@gmail.com>
2024-03-09 02:48:35 +0700

    update_data

--- utils.py
+++ utils.py
@@ -6,9 +6,6 @@ def exchange_rate(URL,date = ""):
     data_list =[]
     res = requests.request('POST',URL)
     soup = BeautifulSoup(res.text,'html.parser')
-<<<<<<< HEAD
-    text = soup.find('table',class_='table')
-=======
     data_list= {
         'exchange_rate' : getCurrencyData(soup)
     }
@@ -19,34 +16,15 @@ def exchange_rate(URL,date = ""):
 def getCurrencyData(soup):
     text = soup.find('table',class_='table')
     data_list ={}
->>>>>>> mengkim
     if text:
     # Iterate through rows and print exchange rates
         for row in text.find_all('tr'):
             columns = row.find_all('td')
-<<<<<<< HEAD
-=======
             
->>>>>>> mengkim
             if len(columns) >= 2:
                 currency = columns[0].text.strip()
                 toCurrency,bid = columns[1].text.strip().split()
                 toCurrency,ask = columns[2].text.strip().split()
-<<<<<<< HEAD
-                
-                data = {
-                    currency : f"{currency}/{toCurrency}",
-                    'bid' : bid.replace(',',''),
-                    'ask' : ask.replace(',','')
-                    
-                }
-                data_list.append(data)
-    else:
-        print("Unable to find data container (tbody) on the webpage.")
-    #convert dictionary to json_data
-    json_data = json.dumps(data_list)
-    return(json_data)
-=======
                 if toCurrency == 'KHR':
                     data_currency ={
                         currency : {
@@ -60,4 +38,3 @@ def getCurrencyData(soup):
         print("Unable to find data container (tbody) on the webpage.")
 
     return data_list
->>>>>>> mengkim
