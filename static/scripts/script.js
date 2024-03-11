e8bd20eeac5d41f361fbdcc9158cfc025d6c6e50
mengkim168 <mengkimtang@gmail.com>
2024-03-12 02:51:20 +0700

    fix_convertor_amount

---
+++ static/scripts/script.js
@@ -0,0 +1,17 @@
+function amount_convertor(amount,to_bid,to_ask){
+    to_bid = to_bid.split("&");
+    to_ask = to_ask.split("&");
+    let ask,bid;
+    if (to_bid[0] == to_ask[0]){
+        ask = parseFloat(to_ask[0]);
+        bid = parseFloat(to_bid[0]);
+    }else{
+        ask = parseFloat(to_ask[1]);
+        bid = parseFloat(to_bid[0]);
+    }
+    console.log(ask);
+    let converted = amount*((bid)/(ask));
+    // format float by setpoint (0.00)
+    converted_amount = converted.toFixed(2)
+    return converted_amount
+}
