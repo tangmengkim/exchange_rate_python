8ab5853f40065c79e032624d5a16a8d0561c0d07
mengkim168 <mengkimtang@gmail.com>
2024-03-14 16:15:30 +0700

    update_style

--- views.py
+++ views.py
@@ -2,28 +2,29 @@ from flask import Blueprint, render_template, request
 from utils import exchange_rates
 import json
 
-
 views_p = Blueprint(__name__,'views')
 
 @views_p.route('/', methods=['GET'])
 def index():
     exchange_rate = json.loads(exchange_rates())
     if exchange_rate['status_code'] == 200:
-        return render_template('index.html', data=exchange_rate)
+        return render_template('index.html', data = exchange_rate )
     else:
         return ('no internet connection')
 
-
-@views_p.route('/edit', methods=['POST'])
+@views_p.route('/edit', methods = ['POST'])
 def process_data():
     if request.method == 'POST':
         # Access form data
         date_status = request.form.get('date_input')
 
         # Process the data and load data as JSON
-        exchange_rate = json.loads(exchange_rates(date_status))
+        exchange_rate = json.loads(exchange_rates( date_status ))
         if exchange_rate['status_code'] == 200:
 
             return render_template('index.html',data=exchange_rate)
         else:
             return ('no internet connection')
+@views_p.route('/test')
+def testing():
+    return render_template('test.html')
