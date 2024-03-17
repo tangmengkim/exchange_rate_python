513ef7a77ebbc6ca6de14a4757653007097211b2
mengkim168 <mengkimtang@gmail.com>
2024-03-18 00:14:37 +0700

    update_converter_currency

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,7 +1,20 @@
 function amount_convertor(amount,to_bid,to_ask){
-    to_bid = to_bid.split("&");
-    to_ask = to_ask.split("&");
+    var toBid_selected = to_bid.options[to_bid.selectedIndex];
+    var toAsk_selected = to_ask.options[to_ask.selectedIndex];
+    var toBidCurrency = toBid_selected.textContent;
+    var toAskCurrency = toAsk_selected.textContent;
+    var toBidSymbol = symbolCurrency(toBidCurrency);
+    var toAskSymbol = symbolCurrency(toAskCurrency);
+    amount = parseFloat(amount);
+    
+    // To Split dataValue as array ###&#### -> [###, ####]
+    to_bid = toBid_selected.value.split("&");
+    to_ask = toAsk_selected.value.split("&");
+
+    // To check selection
+    if (to_ask != "" && to_bid != "" && amount) {
     let ask,bid;
+    // To check if 2selector is the same value
     if (to_bid[0] == to_ask[0]){
         ask = parseFloat(to_ask[0]);
         bid = parseFloat(to_bid[0]);
@@ -9,14 +22,43 @@ function amount_convertor(amount,to_bid,to_ask){
         ask = parseFloat(to_ask[1]);
         bid = parseFloat(to_bid[0]);
     }
-    console.log(ask);
+    // calculator
     let converted = amount*((bid)/(ask));
     // format float by setpoint (0.00)
-    converted_amount = converted.toFixed(2)
-    return converted_amount
-}
+    converted_amount = converted.toFixed(2);
+    amount = amount.toFixed(2);
+    // set converted amount to string
+    converted_amount_text = `${amount} ${toBidSymbol} = ${converted_amount}  ${toAskSymbol} `;
+
+    return converted_amount_text;
+    } 
+    else return "";
+    }
 
 function swap_currency(){
-    alert("test");
-    return false;
+    var to_ask = document.getElementById("to_ask");
+    var to_bid = document.getElementById("to_bid");
+    var swap = to_ask.value;
+    to_ask = to_bid.value;
+    to_bid = swap ;
+    document.getElementById("to_ask").value = to_ask;
+    document.getElementById("to_bid").value = to_bid;
+    document.getElementById("input-amount").value = "";
+    document.getElementById("output-amount").value = "";
 }
+
+function symbolCurrency(currency){
+    const symbol={
+        "KHR" : "៛",
+        "USD" : "$",
+        "THB" : "฿",
+        "EUR" : "€",
+        "AUD" : "$",
+        "VND" : "₫",
+        "CAD" : "$",
+        "LAK" : "₭",
+        "JPY" : "¥",
+        "GBP" : "£",
+        };
+    return symbol[currency];
+    }
